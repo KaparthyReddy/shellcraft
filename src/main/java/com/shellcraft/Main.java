29c28366c4f7818816fa191f44aecde1b18e241a
Kaparthy Reddy <166050493+KaparthyReddy@users.noreply.github.com>
2026-09-11 10:27:39 +0530

    Create Main.java

---
+++ src/main/java/com/shellcraft/Main.java
@@ -0,0 +1,9 @@
+package com.shellcraft;
+
+import com.shellcraft.shell.ShellSession;
+
+public class Main {
+    public static void main(String[] args) {
+        new ShellSession().run();
+    }
+}
